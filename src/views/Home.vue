2cff68f757d40a6011dd3d001e18b24536af75a2
daniela <rod.gondaniela0511@gmail.com>
2020-08-27 00:44:44 -0400

    chequeo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,13 @@
     <Dashboard />
     <Producto />
     <DetalleProduc />
-    <Menu/>
+
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Dashboard from '@/components/Dashboard.vue'
-import Menu from '@/components/Menu.vue'
 import Producto from '@/components/Producto.vue'
 import DetalleProduc from '@/components/DetalleProduc.vue'
 
@@ -18,7 +17,6 @@ export default {
     name: 'home',
     components: {
         Dashboard,
-        Menu,
         Producto,
         DetalleProduc
     }
